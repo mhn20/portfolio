e9f477107fbffdc264acab6cc94ac8720c0c9a06
mhn20 <mahmud_i@rocketmail.com>
2026-06-13 07:27:01 +0700

    new update

--- script-detail.js
+++ script-detail.js
@@ -337,7 +337,11 @@ function changeLanguage(lang) {
     document.querySelectorAll('[data-i18n]').forEach(el => {
         const key = el.getAttribute('data-i18n');
         if (translations[lang][key]) {
-            el.innerHTML = translations[lang][key];
+            if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA') {
+                el.placeholder = translations[lang][key];
+            } else {
+                el.innerHTML = translations[lang][key];
+            }
         }
     });
     
